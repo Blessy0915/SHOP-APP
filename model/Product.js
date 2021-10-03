3d65a95989fee455509917e13baf13d6161b1689
Blessy0915 <blessyferns2597@gmail.com>
2021-10-03 11:51:49 +0530

    structure/blueprint model added for product

---
+++ model/Product.js
@@ -0,0 +1,10 @@
+export class Product {
+    constructor(id, ownerID, title, description, imageURL, price){
+        this.id = id
+        this.ownerID = ownerID
+        this.title = title
+        this.description = description
+        this.imageURL = imageURL
+        this.price = price
+    }
+}
