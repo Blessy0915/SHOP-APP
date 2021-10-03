65b362427a5743659d6b3993e5663be657cb8610
Blessy0915 <blessyferns2597@gmail.com>
2021-10-03 12:07:17 +0530

    updated product model

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,10 @@
 export class Product {
-    constructor(id, ownerID, title, description, imageURL, price){
+    constructor(id, ownerID, title, imageURL, description, price){
         this.id = id
         this.ownerID = ownerID
         this.title = title
-        this.description = description
         this.imageURL = imageURL
+        this.description = description
         this.price = price
     }
 }
