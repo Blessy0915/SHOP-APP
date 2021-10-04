1f711e537fe3ce1b84d54c4aef0edf3caf63d4b7
Blessy0915 <blessyferns2597@gmail.com>
2021-10-04 23:11:57 +0530

    delete product actions & reducers

--- store/reducers/product.js
+++ store/reducers/product.js
@@ -1,4 +1,5 @@
 import PRODUCTS from '../../data/dummy-data'
+import { DELETE_PRODUCT } from '../actions/product'
 
 const initailState = {
     availableProducts : PRODUCTS,
@@ -6,7 +7,19 @@ const initailState = {
 }
 
 const productsReducer = ( state=initailState, action ) => {
-    return state
+    switch(action.type){
+        case DELETE_PRODUCT : {
+            const productID = action.payload.productID
+            return {
+                ...state,
+                availableProducts : state.availableProducts.filter(product => product.id != productID),
+                userProducts : state.userProducts.filter(product => product.id != productID)
+            }
+        }
+        default : {
+            return state
+        }
+    }
 }
 
 export default productsReducer
