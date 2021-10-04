098656cf694631a7822bd8d671d1ab3d81e245ff
Blessy0915 <blessyferns2597@gmail.com>
2021-10-04 22:05:20 +0530

    user products screen layout

--- screens/user/UsersProductsScreen.js
+++ screens/user/UsersProductsScreen.js
@@ -4,17 +4,33 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import CustomHeaderButton from '../../components/UI/HeaderButton'
 import { useSelector } from 'react-redux'
 import ProductItem from '../../components/shop/ProductItem'
+import { FlatList } from 'react-native-gesture-handler'
 
 const UsersProductsScreen = (props) => {
     const userProducts = useSelector(state => state.products.userProducts)
     return (
-        <View></View>
+        <FlatList data={userProducts}
+                  keyExtractor={(item,index) => item.id}
+                  renderItem={(itemData) => ( 
+                        <ProductItem imageURL={itemData.item.imageURL}
+                        price={itemData.item.price}
+                        description={itemData.item.description} 
+                        addToCart={() => {
+                            dispatch(cartActions.addToCart(itemData.item))
+                        }}
+                        viewDetails={() => {
+                            props.navigation.navigate('ProductDetail', {
+                            productID : itemData.item.id,
+                            productTitle : itemData.item.title
+                            })
+                        }}/>
+                  )}/>
     )
 }
 
 UsersProductsScreen.navigationOptions = (navData) => {
     return {
-        headerTitle : "ADMIN PANEL",
+        headerTitle : "YOUR PRODUCTS",
         headerLeft : () => (
             <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
                 <Item title="MENU"
