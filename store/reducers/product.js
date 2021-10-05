7df02ab0ae8eb887c55a63521a9192ba16040519
Blessy0915 <blessyferns2597@gmail.com>
2021-10-05 13:40:58 +0530

    reducer logic for creating product

--- store/reducers/product.js
+++ store/reducers/product.js
@@ -1,5 +1,6 @@
 import PRODUCTS from '../../data/dummy-data'
-import { DELETE_PRODUCT } from '../actions/product'
+import Product from '../../models/Product'
+import { DELETE_PRODUCT, UPDATE_PRODUCT, CREATE_PRODUCT } from '../actions/product'
 
 const initailState = {
     availableProducts : PRODUCTS,
@@ -14,6 +15,22 @@ const productsReducer = ( state=initailState, action ) => {
                 ...state,
                 availableProducts : state.availableProducts.filter(product => product.id != productID),
                 userProducts : state.userProducts.filter(product => product.id != productID)
+            }
+        }
+        case CREATE_PRODUCT : {
+            const product = action.payload.product
+            const newProduct = new Product(
+                new Date().toString(),
+                'u1',
+                product.title,
+                product.imageURL,
+                product.description,
+                product.price
+            )
+            return {
+                ...state,
+                availableProducts : state.availableProducts.concat(newProduct),
+                userProducts : state.userProducts.concat(newProduct)
             }
         }
         default : {
