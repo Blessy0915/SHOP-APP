843e526c4a9f3221e25c515fd381f6ed49866b1d
Blessy0915 <blessyferns2597@gmail.com>
2021-10-05 11:42:18 +0530

    edit/add screen layout & styling

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { View, TextInput } from 'react-native'
+import { View, TextInput, ScrollView, Text, StyleSheet } from 'react-native'
 
 const EditProductScreen = () => {
     return (
-        <View>
-            <Text>Edit</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.screen}>
+                <View style={styles.container}>
+                    <Text styles={styles.label}>TITLE</Text>
+                    <TextInput style={styles.input}/>
+                </View>
+                <View style={styles.container}>
+                    <Text>PRICE</Text>
+                    <TextInput  style={styles.input}/>
+                </View>
+                <View style={styles.container}>
+                    <Text>IMAGE URL</Text>
+                    <TextInput  style={styles.input}/>
+                </View>
+                <View style={styles.container}>
+                    <Text>DESCRIPTION</Text>
+                    <TextInput  style={styles.input}/>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
@@ -15,4 +32,23 @@ EditProductScreen.navigationOptions = (navData) => {
         headerTitle : id ? 'EDIT PRODUCT' : 'ADD PRODUCT',
     }
 }
+
+const styles = StyleSheet.create({
+    screen : {
+        margin : 20
+    },
+    container : {
+        marginVertical : 10
+    },
+    label : {
+        fontSize : 20,
+        marginVertical : 8
+    },
+    input : {
+        paddingHorizontal : 2,
+        paddingVertical : 5,
+        borderBottomColor : '#ccc',
+        borderBottomWidth : 1
+    }
+})
 export default EditProductScreen
